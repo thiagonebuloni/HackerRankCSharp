bcac6eb20fd4718a598f3277e5a7739171c05a65
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-05-10 22:30:32 -0300

    add GridChallenge

--- GridChallenge/Program.cs
+++ GridChallenge/Program.cs
@@ -1,44 +1,55 @@
-﻿namespace GridChallenge;
+﻿using System.IO;
+
+namespace GridChallenge;
 
 public class Program
 {
 
     public static void Main(string[] args)
     {
-        List<string> grid = new List<string>{ "ebacd", "fghij", "olmkn", "trpqs", "xywuv" };
+        // List<string> newList = new List<string>{ "eabcd", "fghij", "olkmn", "trpqs", "xywuv" };
+        // List<string> newList2 = new List<string>{ "abc", "aed", "efg"};
+        List<string> newList = new List<string>{ "kc", "iu"};
+        // List<string> newList2 = new List<string>{ "uxf", "vof", "hmp"};
 
-        gridChallenge(grid);
+        // List<string> newList = new List<string>(fileToLists());
+        Console.WriteLine($"{gridChallenge(newList)}");
+        // Console.WriteLine($"{gridChallenge(newList2)}");
+        // Console.WriteLine($"newList = { newList }");
     }
 
+    
     public static string gridChallenge(List<string> grid)
     {
-        string result = "YES";
-
         int rows = grid.Count;
+        foreach (var c in grid) Console.WriteLine($"{c}");
+
+        int columns = grid[0].Length;
+
         List<string> gridSorted = new List<string>();
 
         for (int i = 0; i < rows; i++)
         {
             string sorted = grid[i];
             sorted = String.Concat(sorted.OrderBy(c => char.ToLower(c)).ThenBy(c => c));
+            Console.WriteLine($"{sorted}");
             gridSorted.Add(sorted);
         }
         
 
         foreach (var row in gridSorted) Console.WriteLine($"{row}");
 
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < columns; i++)
         {
             for (int j = 0; j < rows - 1; j++) {
-                if (grid[j][i] > grid[j + 1][i])
+                if (gridSorted[j][i] > gridSorted[j + 1][i])
                 {
-                    result = "NO";
-                    break;
+                    Console.WriteLine($"gridSorted[j][i] > gridSorted[j+1][i]{gridSorted[j][i]} > {gridSorted[j + 1][i]}");
+                    return "NO";
                 }
             }
         }
-        Console.WriteLine($"{result}");
 
-        return result;
+        return "YES";
     }
 }
