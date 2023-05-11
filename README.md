d09d609be19637ee8d8217b3c2818bcbd8dbed88
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-05-11 18:49:56 -0300

    add RecursiveDigitSum

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Exercícios Hacker Rank em C#
+
+[Link](https://www.hackerrank.com/interview/preparation-kits/one-week-preparation-kit/one-week-day-four/challenges)
---
+++ RecursiveDigitSum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RecursiveDigitSum;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string n = "9875";
+        int k = 4;
+        Console.WriteLine($"{superDigit(n, k)}");
+    }
+
+    public static int superDigit(string n, int k)
+    {
+        List<long> digits = new List<long>();
+        long sum = 0;
+
+        foreach (char digit in n)
+        {
+            digits.Add(int.Parse(digit.ToString()));
+            Console.WriteLine($"{digit}");
+        }
+
+        foreach (long num in digits) sum += num;
+
+        Console.WriteLine($"sum = {sum}");
+        Console.ReadLine();
+
+        sum *= k;
+
+        if (sum > 9) return superDigit(Convert.ToString(sum), 1);
+
+        return Convert.ToInt32(sum);
+
+    }
+
+    
+}
