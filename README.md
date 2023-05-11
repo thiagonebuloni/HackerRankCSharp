9f2acfbf0dfc0a096914e688dc4155cfd9d19fd1
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-05-10 22:33:30 -0300

    add README and TowerBreakers

---
+++ README.md
@@ -0,0 +1,1 @@
+# Exercícios Hacker Rank em C#
--- TowerBreakers/Program.cs
+++ TowerBreakers/Program.cs
@@ -5,17 +5,7 @@ public class Program
 
 public static int towerBreakers(int n, int m)
     {
-        bool turns = true;
-        int[] towers;
-        
-        for (int i = 0; i < n; i++)
-        {
-            towers.Append(m);
-        }
-        
-        while (true)
-        {
-            if (turns)
-        }
+        if (m == 1 || n % 2 == 0) return 2;
+        else return 1;
     }
 }
